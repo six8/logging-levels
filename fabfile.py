2679d685ececb7a4513d9aea6a764f6910845b48
Mike Thornton <msthornton@gmail.com>
2014-01-27 16:03:45 -0600

    Changed fabfile to use git flow for release

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,14 @@
 from fabric.api import local, task, abort, settings
 from clom import clom
+from fabric.colors import green
+from fabric.utils import puts
+from fabric.operations import prompt
 
 @task
 def release():
     """
     Release current version to pypi
-    """
+    """    
 
     with settings(warn_only=True):
         r = local(clom.git['diff-files']('--quiet', '--ignore-submodules', '--'))
@@ -15,11 +18,17 @@ def release():
 
     version = open('VERSION.txt').read().strip()
 
-    print('Releasing %s...' % version)
-    local(clom.git.tag(version, a=True, m='Release %s' % version))
-    local(clom.git.push('origin', 'HEAD'))
-    local(clom.git.push('origin', version))
-    local(clom.python('setup.py', 'sdist', 'upload'))
+    puts(green('Releasing %s...' % version))
+    local(clom.git.flow.release.start(version))
+
+    # Can't use spaces in git flow release messages, see https://github.com/nvie/gitflow/issues/98
+    local(clom.git.flow.release.finish(version, m='Release-%s' % version))
+
+    if prompt('Push to origin?'):
+        local(clom.git.push('origin', 'master', 'develop').with_opts(tags=True))        
+
+        if prompt('Push to pypi?'):
+            local(clom.python('setup.py', 'sdist', 'upload'))
 
 @task
 def register():
